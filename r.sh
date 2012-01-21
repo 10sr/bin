5813c701f9fb122b02bf1a56d668f804f4d93f29
10sr <8slashes+git@gmail.com>
2012-01-21 18:38:01 +0900

    add archive() not tested yet

--- r.sh
+++ r.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
+# todo : examine what happens about simlink and enable archive().
 
 src="$HOME/.xdg-dirs/Dropbox/"
 dst="$HOME/bu/Dropbox/"
 log="$HOME/bu/tb/cronrsync.log"
 errorlog="$HOME/bu/tb/cronrsyncerror.log"
+pack_cmd="7z a"
+pack_out="%s.7z"
 
 export DISPLAY=:0.0
 export LANG=ja_JP.UTF-8
@@ -14,6 +17,7 @@ todir="$dst/newest/"
 
 size_fromdir=$(du -s "$fromdir" | awk '{print $1}')
 header=$(printf "\nCRON rsync $fromdir > $todir: run at "; date;)
+date_num=`date +%Y%m%d-%H%M%S`
 returncode=0
 message=""
 
@@ -23,6 +27,15 @@ notify(){                       # gnome notify first argument
     echo "message: $1" >&10
     sleep 1
     exec 10>&-
+}
+
+archive(){                      # $ archive dir
+    dir="$dst"
+    files="$(find $(echo ${dir}/*) -maxdepth 0 '!' -name newest -type d)"
+    arc="`format ${dir}/${pack_out} ${date_num}`"
+    test `echo $files | wc -w` -gt 100 &&
+    ${pack_cmd} "${arc}" ${files} &&
+    echo rm -rf ${files}
 }
 
 if [ "$size_fromdir" -lt 1000 ]; then  # size is less than 1MB
@@ -37,7 +50,7 @@ else
     mkdir -p $todir
     {
         echo "$header"
-        rsync -avh --stats --delete --backup --backup-dir=../`date +%Y%m%d-%H%M%S` $fromdir $todir
+        rsync -avh --stats --delete --backup --backup-dir=../${date_num} $fromdir $todir
     } >>$log 2>>$errorlog
     returncode=$?
     if [ $returncode -eq 0 ]; then
