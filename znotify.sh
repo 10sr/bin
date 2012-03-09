85d392f6a0c9ec1e24225fe8ce67b6196e383dc6
10sr <8slashes+git@gmail.com>
2012-03-09 16:22:00 +0900

    fix bug

--- znotify.sh
+++ znotify.sh
@@ -3,7 +3,7 @@
 export DISPLAY=:0.0
 export LANG=ja_JP.UTF-8
 
-.exec 10> >(zenity --notification --listen --window-icon "/usr/share/pixmaps/none.png")
+exec 10> >(zenity --notification --listen --window-icon "/usr/share/pixmaps/none.png")
 echo "visible: false" >&10
 
 echo "message: $1" >&10
