bdd2724b08152d80ef88e142dd2c308420306e27
10sr <8slashes+git@gmail.com>
2012-04-07 23:51:42 +0900

    generate patch of jp106.map

--- arch/updatefiles.sh
+++ arch/updatefiles.sh
@@ -16,9 +16,13 @@ do
     cp -fvu $i ./$(basename $i)
 done
 
-map=/usr/share/kbd/keymaps/i386/qwerty/myjp106.map
-zcat ${map}.gz >./$(basename ${map}) &&
-echo "\`$map\' -> \`./$(basename $map)\'"
+mapdir=/usr/share/kbd/keymaps/i386/qwerty
+zcat ${mapdir}/myjp106.map.gz >./myjp106.map &&
+    zcat ${mapdir}/jp106.map.gz >./jp106.map &&
+    {
+        diff -u ./jp106.map ./myjp106.map >mymap.diff
+        echo "Make myjp106 patch."
+    }
 
 pacman -Qqe | grep -vx "$(pacman -Qqm)" > ./pkg.lst &&
 echo "Make pkg.lst."
