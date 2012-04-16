c8dfe8fb3e31af824d58d83b8bdb6d7cf04fc60a
10sr <8slashes+git@gmail.com>
2012-04-16 23:11:32 +0900

    cleanse rgit.sh

--- rgit.sh
+++ rgit.sh
@@ -2,47 +2,43 @@
 
 src="$HOME/.xdg-dirs/Dropbox/"
 dst="$HOME/.backup/Dropbox_git/"
-log="${dst}/cronrsync.log"
+log="${dst}/rgit.log"
 
 export DISPLAY=:0.0
 export LANG=ja_JP.UTF-8
 export LC_TIME=C
+export GIT_DIR=$dst/rep
+export GIT_WORK_TREE=.
 
-#fromdir="$src/"        # must end with "/"
-#todir="$dst/"
-
-header=$(printf "\nCRON rsync $fromdir > $todir: run at "; date;)
+header=$(printf "\nrgit $src > $dst: run at "; date;)
 returncode=0
 message=""
 mkdir -p "$dst"
 
-rsync_run(){
-    echo "$header"
-    rsync -avh --stats --delete "$fromdir" "$todir" 2>&1
-}
-
-export GIT_DIR=$dst/rep
-export GIT_WORK_TREE=$src
 git_ci(){
+    echo "$header"
     cd $src
     test -d $dst/rep || git init
-    git add -A *
-    test -n "`git status -s`" && git commit -m "$0 commit"
+    git add -v -A .
+    if test -n "`git status -s -uno`"
+    then
+        git commit -m "$0 commit"
+    else
+        return 0
+    fi
 }
 
-git_ci
+{
+    git_ci
+    returncode=$?
+} | tee -a $log
 
-#rsync_run | tee -a "$log"
-#returncode=$?
-
-# if [ $returncode -eq 0 ]; then
-#     message="rsync $fromdir > $todir: Done successfully."
-#     echo "$message"
-#     git_ci
-# else
-#     message="!!! rsync $fromdir > $todir: Something wrong happened!"
-#     echo "$message" 1>&2
-# fi
+if test $returncode -eq 0
+then
+    message="Commit done successfully."
+else
+    message="!!! Commit failed. Check log."
+fi
 
 if type notify-send >/dev/null 2>&1
 then
