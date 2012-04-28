776bf6727743af3f41ecaff3d5d63af0347ded47
10sr <8slashes+git@gmail.com>
2012-04-29 01:10:59 +0900

    do not copy myjp106.map if up-to-date file exists

--- arch/updatefiles.sh
+++ arch/updatefiles.sh
@@ -17,12 +17,16 @@ do
 done
 
 mapdir=/usr/share/kbd/keymaps/i386/qwerty
+
+test ${mapdir}/myjp106.map.gz -nt ./myjp106.map &&
 zcat ${mapdir}/myjp106.map.gz >./myjp106.map &&
-    zcat ${mapdir}/jp106.map.gz >./jp106.map &&
-    {
-        diff -u ./jp106.map ./myjp106.map >mymap.diff
-        echo "Make myjp106 patch."
-    }
+{
+    test -f ./jp106.map || 
+    zcat ${mapdir}/jp106.map.gz >./jp106.map
+
+    diff -u ./jp106.map ./myjp106.map >mymap.diff
+    echo "Make myjp106 patch."
+}
 
 pacman -Qqe | grep -vx "$(pacman -Qqm)" > ./pkg.lst &&
 echo "Make pkg.lst."
