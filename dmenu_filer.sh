829aead23820d30e1bb09fb61b837d6633128a90
10sr <8slashes+git@gmail.com>
2012-05-13 15:14:14 +0900

    accept args

--- dmenu_filer.sh
+++ dmenu_filer.sh
@@ -1,20 +1,23 @@
-#!/bin/sh
+#!/bin/bash
 
 dir=.
 
+args=("$@")
+
 dmenu_dir(){
-    ls -1 --color=never "$1" | dmenu -fn '-*-dejavu sans mono-*-r-*-*-11-*-*-*-*-*-*-*' -p "$1" -l 10 -i
+    ls -1 --color=never "$1" | dmenu "${args[@]}" -p "$1"
 }
 
 get_file(){
     test -e $1 || return 1
+
     if test -f $1
     then
         exec xdg-open $1
     else
         file=$(dmenu_dir $1)
         test -n "$file" || return 1
-        get_file "$1/${file}"
+        get_file "$(realpath "$1/${file}")"
     fi
 }
 
