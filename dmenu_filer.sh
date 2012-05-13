b7f246ea60b217315b15b90fac3a102ac7ee8d08
10sr <8slashes+git@gmail.com>
2012-05-13 14:31:02 +0900

    update dmenu_filer

--- dmenu_filer.sh
+++ dmenu_filer.sh
@@ -3,16 +3,19 @@
 dir=.
 
 dmenu_dir(){
-    ls -1 --color=never "$1" | dmenu
+    ls -1 --color=never "$1" | dmenu -fn '-*-dejavu sans mono-*-r-*-*-11-*-*-*-*-*-*-*' -p "$1" -l 10 -i
 }
 
 get_file(){
+    test -e $1 || return 1
     if test -f $1
     then
-        xdg-open $1
+        exec xdg-open $1
     else
-        get_file "$1/$(dmenu_dir $1)"
+        file=$(dmenu_dir $1)
+        test -n "$file" || return 1
+        get_file "$1/${file}"
     fi
 }
 
-get_file ~/
+get_file ~
