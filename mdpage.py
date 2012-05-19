a92e52df4ed785aaae72b365d691bdfcf7ba2eda
10sr <8slashes+git@gmail.com>
2012-05-20 00:37:07 +0900

    update mdpage

--- mdpage.py
+++ mdpage.py
@@ -3,16 +3,23 @@
 import os
 
 def make_file_list():
+    """Return list of md file without .md extension"""
     l = os.listdir()
     return (f for f, e in map(os.path.splitext, l) \
-                if e == ".py" and os.path.isfile(f + e))
+                if e == ".md" and os.path.isfile(f + e))
 
-def is_md_file(f):
-    e = os.path.splitext(f)[1]
-    return e == ".py"
+def is_updated(f):
+    """Return True if html file is not exist or markdown file is newer than htmls"""
+    return not os.path.isfile(f + ".html") or file_newer(f + ".md", f + ".html")
+
+def file_newer(f1, f2):
+    """Return True if f1 is newer than f2"""
+    return os.path.getmtime(f1) > os.path.getmtime(f2)
 
 def main():
     for f in make_file_list():
         print(f)
+        if is_updated(f):
+            print("%s updated." % f)
 
 main()
