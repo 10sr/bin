cf074c40adca1d8a075bb285d380d9ce3834ad87
10sr <8slashes+git@gmail.com>
2012-05-20 00:06:20 +0900

    update mdpage, is_md_file becomes unused

--- mdpage.py
+++ mdpage.py
@@ -4,7 +4,8 @@ import os
 
 def make_file_list():
     l = os.listdir()
-    return (f for f in l if is_md_file(f) and os.path.isfile(f))
+    return (f for f, e in map(os.path.splitext, l) \
+                if e == ".py" and os.path.isfile(f + e))
 
 def is_md_file(f):
     e = os.path.splitext(f)[1]
