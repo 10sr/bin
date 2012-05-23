0e7d2806909c993122f2e501d373690c5f3d56aa
10sr <8slashes+git@gmail.com>
2012-05-23 19:50:40 +0900

    play the first station

--- shoutsearch.py
+++ shoutsearch.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 shoutcast = "http://www.shoutcast.com/"
-player = "mpg123 -C -v --title"
+player = "mpg123 -C -v --title -@"
 
 from urllib.request import urlopen
 #from xml.dom.minidom import parse
@@ -9,13 +9,34 @@ from html.parser import HTMLParser
 from subprocess import call
 import os
 
+class MyHTMLParser(HTMLParser):
+    playlists = []
+
+    def handle_starttag(self, tag, attrs):
+        if tag != "a" : return
+
+        pstr = "http://yp.shoutcast.com/sbin/tunein-station.pls"
+
+        for a in attrs :
+            if a[0] == "href" and pstr in a[1] :
+                self.playlists.append(a[1])
+
 def play(url):
-    call(player + " " + media, shell=True)
+    call(player + " " + url, shell=True)
 
 def search(word):
-    url = shoutcast + "Internet-Radio/" + work
+    return shoutcast + "Internet-Radio/" + word
 
 def get_playlists(url):
     data = urlopen(url)
-    dom = parse(data)
-    media = dom.getElementsByClassName("")
+    parser = MyHTMLParser()
+    parser.feed(data.read().decode("utf-8"))
+    return parser.playlists
+
+
+def main():
+    url = search("alternative")
+    p = get_playlists(url)
+    play(p[0])
+
+main()
