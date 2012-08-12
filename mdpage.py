202c1b06cd523b2a0723d5533964e9c075617d57
10sr <8slashes+git@gmail.com>
2012-08-13 00:43:00 +0900

    seem to work well after refactoring

--- mdpage.py
+++ mdpage.py
@@ -18,113 +18,35 @@ except ImportError :
 
 class MDPage:
     mdc = None
+    hf = None
+    fl = None
 
     cur_time = None
 
     enc = "utf-8"
     dec = "utf-8"
 
-    file_list = []
-    dir_list = []
-    updated_list = []
-
-    header_file = ".header.html"
-    footer_file = ".footer.html"
-    header = None
-    footer = None
-    header_def = """<?xml version="1.0" encoding="UTF-8"?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
-"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-<meta http-equiv="Content-Style-Type" content="text/css" />
-<!-- <link rel="stylesheet" href="style.css" type="text/css" /> -->
-<title>${name} | title</title>
-</head>
-<body>
-<h1 class="title">${name} | <a href="index.html">title</a></h1>
-<h2 class="subtitle">subtitle</h2>
-"""
-    footer_def = """</body>
-</html>
-<!-- Last update : ${time} -->
-"""
-
-    list_file = ".files.lst"
-
     menu = ""
 
     update_all = False
-    header_updated = False
-    footer_updated = False
-    filelist_updated = False
 
     def __init__(self):
 
-        self.mdc = MDConv()
-
         self.cur_time = strftime("%a, %d %b %Y %H:%M:%S %z")
 
-        l = os.listdir()
-        self.file_list = [f for f, e in map(os.path.splitext, l) \
-                          if not f.startswith(".") and e == ".md" and os.path.isfile(f + e)]
-        self.file_list.sort()
-
-        self.dir_list = [d + "/" for d in l if os.path.isdir(d) and not d.startswith(".")]
-        self.dir_list.sort()
-
-        self.updated_list = [f for f in self.file_list if self.is_md_updated(f)]
-
-    def is_md_updated(self, f):
-        """Return True if html file is not exist or markdown file is newer than htmls"""
-        return not os.path.isfile(f + ".html") or self.is_file_newer(f + ".md", f + ".html")
-
-    def is_file_newer(self, f1, f2):
-        """Return True if f1 is newer than f2"""
-        return os.path.getmtime(f1) > os.path.getmtime(f2)
-
-    def is_file_updated(self, file):
-        """judge if file is newer than any of html file in filelist. file must be exist."""
-        for f in self.file_list :
-            if os.path.isfile(f + ".html") and self.is_file_newer(file, f + ".html") :
-                return True
-
-        return False
-
-    def gen_header(self):
-        """get header or generate file newly if needed"""
-        if os.access(self.header_file, os.R_OK) :
-            fd = open(self.header_file, mode="r", encoding=self.enc)
-            self.header = fd.read()
-            fd.close()
-        else :
-            fd = open(self.header_file, mode="w", encoding=self.dec)
-            fd.write(self.header_def)
-            fd.close()
-            self.header = self.header_def
-
-    def gen_footer(self):
-        """get footer or generate file newly if needed"""
-        if os.access(self.footer_file, os.R_OK) :
-            fd = open(self.footer_file, mode="r", encoding=self.enc)
-            self.footer = fd.read()
-            fd.close()
-        else :
-            fd = open(self.footer_file, mode="w", encoding=self.dec)
-            fd.write(self.footer_def)
-            fd.close()
-            self.footer = self.footer_def
+        self.mdc = MDConv()
+        self.hf = HeaderFooter()
+        self.fl = FileList()
 
     def gen_menu(self):
         """generate menu ul"""
         s = "<ul class=\"menu\">\n"
 
-        if "index" in self.file_list :
+        if "index" in self.fl.file_list :
             s = s + "<li><a href=\"%s.html\">%s</a></li>\n" % ("index", "index")
-        for f in self.dir_list :
+        for f in self.fl.dir_list :
             s = s + "<li><a href=\"%sindex.html\">%s</a></li>\n" % (f, f)
-        for f in self.file_list :
+        for f in self.fl.file_list :
             if f != "index" :
                 s = s + "<li><a href=\"%s.html\">%s</a></li>\n" % (f, f)
 
@@ -132,92 +54,59 @@ class MDPage:
 
         self.menu = s
 
-    def update_filelist(self):
-        """update list_file"""
-        fd = open(self.list_file, mode="w", encoding=self.dec)
-        for d in self.dir_list :
-            fd.write(d + "\n")
-        for f in self.file_list :
-            fd.write(f + "\n")
-        fd.close
-
-    def is_filelist_updated(self):
-        """judge if there is any file newly created or removed sinse last update. list_file must be exist."""
-        fd = open(self.list_file, mode="r", encoding=self.enc)
-        oldlist = fd.read().split("\n")[:-1]
-        fd.close()
-        if set(oldlist) == set(self.file_list + self.dir_list) :
-            return False
-        else :
-            return True
-
     def autoremove(self):
         for f in os.listdir() :
-            if not os.path.isdir(f) and f.endswith(".html") and not f.startswith(".") and \
+            if not os.path.isdir(f) and f.endswith(".html") and \
+                    not f.startswith(".") and \
                     not os.path.splitext(f)[0] in self.file_list :
                 print("rm : %s." % f)
                 os.remove(f)
 
     def make(self):
         self.check()
-        self.run()
-
-    def check(self):
-        if not os.path.isfile(self.header_file) :
-            print("Header file does not exist.")
-            self.header_updated = True
-        elif self.is_file_updated(self.header_file) :
-            print("Header file is updated.")
-            self.header_updated = True
-
-        if not os.path.isfile(self.footer_file) :
-            print("Footer file does not exist.")
-            self.footer_updated = True
-        elif self.is_file_updated(self.footer_file) :
-            print("Footer file is updated.")
-            self.footer_updated = True
-
-        if not os.path.isfile(self.list_file) :
-            print("List file does not exist.")
-            self.filelist_updated = True
-        elif self.is_filelist_updated() :
-            print("File list is updated.")
-            self.filelist_updated = True
+        self.update()
 
     def print_updated(self):
-        for f in self.updated_list:
-            print("Update : %s" % f)
+        for f in self.fl.updated_list:
+            print("%s.md is to be updated." % f)
 
     def force(self):
         self.check()
         self.update_all = True
         self.run()
 
-    def run(self):
-        """do check() before call this"""
-        if not self.mdc.conv :
-            print("No way to convert md file!", file = stderr)
-            return 1
+    def check(self):
+        """only check, do not create any file"""
+        self.fl.check_filelist_updated(self.enc)
+        self.hf.check_updated(self.fl.file_list)
 
-        if self.update_all or self.header_updated or self.footer_updated or self.filelist_updated :
+    def update(self):
+        """do check() before call this"""
+        if self.mdc.conv == None :
+            print("No way to convert md file!", file = stderr)
+            return
+
+        if self.update_all or \
+                self.hf.header.exist == False or \
+                self.hf.footer.exist == False or \
+                self.hf.header.updated or self.hf.footer.updated :
             print("all files are to be updated.")
-            fl = self.file_list
+            fl = self.fl.file_list
         else :
-            fl = self.updated_list
+            fl = self.fl.updated_list
+
+        if self.update_all or self.fl.updated :
+            self.fl.update_file(self.dec)
 
         if not fl :
             print("No file to update.")
             return
 
-        if self.update_all or self.filelist_updated :
-            self.update_filelist()
-
-        self.gen_header()
-        self.gen_footer()
+        self.hf.set()
         self.gen_menu()
 
-        h_template = Template(self.header)
-        f_template = Template(self.footer)
+        h_template = Template(self.hf.header.s)
+        f_template = Template(self.hf.footer.s)
 
         for f in fl :
             res = self.mdc.conv(f + ".md", self.enc)
@@ -231,11 +120,153 @@ class MDPage:
             htmlfd.close()
             print("%s.md -> %s.html." % (f, f))
 
-class HeaderFooter :
-    pass
-
 class FileList :
-    pass
+    file_list = []
+    dir_list = []
+    updated_list = []
+
+    fname = ".files.lst"
+    updated = None
+
+    def __init__(self) :
+        l = os.listdir()
+        self.file_list = [f for f, e in map(os.path.splitext, l) \
+                              if not f.startswith(".") and \
+                              e == ".md" and os.path.isfile(f + e)]
+        self.file_list.sort()
+
+        self.dir_list = [d + "/" for d in l \
+                             if os.path.isdir(d) and not d.startswith(".")]
+        self.dir_list.sort()
+
+        self.updated_list = [f for f in self.file_list if self.is_md_updated(f)]
+
+    def is_md_updated(self, f):
+        """Return True if html file is not exist or markdown file is newer"""
+        return not os.path.isfile(f + ".html") or \
+            self.is_file_newer(f + ".md", f + ".html")
+
+    def is_file_newer(self, f1, f2):
+        """Return True if f1 is newer than f2"""
+        return os.path.getmtime(f1) > os.path.getmtime(f2)
+
+    def update_file(self, encoding="utf-8"):
+        """update list_file"""
+        fd = open(self.fname, mode="w", encoding=encoding)
+        for d in self.dir_list :
+            fd.write(d + "\n")
+        for f in self.file_list :
+            fd.write(f + "\n")
+        fd.close
+        print("%s generated." % self.fname)
+
+    def check_filelist_updated(self, encoding="utf-8"):
+        """check if there is any file newly created or removed"""
+        if not os.path.isfile(self.fname) :
+            print("%s not exist." % self.fname)
+            self.updated = True
+            return
+
+        fd = open(self.fname, mode="r", encoding=encoding)
+        oldlist = fd.read().split("\n")[:-1]
+        fd.close()
+        if set(oldlist) == set(self.file_list + self.dir_list) :
+            self.updated = False
+        else :
+            self.updated = True
+            print("File list was updated.")
+
+class PageTemplate :
+    """class for header and footer"""
+    fname = None
+    defalut = None
+    exist = None
+    s = None
+    updated = None
+
+    def __init__(self, filename, default) :
+        self.fname = filename
+        self.default = default
+        self.check_exist()
+
+    def set(self, encoding="utf-8") :
+        """set self.s and create file if none"""
+        if self.exist :
+            self.read_file(encoding)
+        else :
+            self.write_file(encoding)
+            self.s = self.default
+
+    def check_exist(self) :
+        if os.access(self.fname, os.R_OK) :
+            self.exist = True
+        else :
+            self.exist = False
+            print("%s not exist." % self.fname)
+
+    def check_updated(self, flist) :
+        """judge if file is newer than any of html file in flist"""
+        if not self.exist :
+            return
+        for f in flist :
+            if os.path.isfile(f + ".html") and \
+                    self.is_file_newer(self.fname, f + ".html") :
+                print("%s is updated." % self.fname)
+                self.updated = True
+                return
+        self.updated = False
+
+    def is_file_newer(self, f1, f2):
+        """Return True if f1 is newer than f2"""
+        return os.path.getmtime(f1) > os.path.getmtime(f2)
+
+    def read_file(self, encoding="utf-8") :
+        fd = open(self.fname, mode="r", encoding=encoding)
+        self.s = fd.read()
+        fd.close()
+
+    def write_file(self, decoding="utf-8") :
+        fd = open(self.fname, mode="w", encoding=decoding)
+        fd.write(self.default)
+        fd.close()
+        print("%s generated." % self.fname)
+
+class HeaderFooter :
+    header = None
+    footer = None
+
+    header_file = ".header.html"
+    footer_file = ".footer.html"
+    HEADER_DEF = """<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<meta http-equiv="Content-Style-Type" content="text/css" />
+<!-- <link rel="stylesheet" href="style.css" type="text/css" /> -->
+<title>${name} | title</title>
+</head>
+<body>
+<h1 class="title">${name} | <a href="index.html">title</a></h1>
+<h2 class="subtitle">subtitle</h2>
+"""
+    FOOTER_DEF = """</body>
+</html>
+<!-- Last update : ${time} -->
+"""
+
+    def __init__(self) :
+        self.header = PageTemplate(self.header_file, self.HEADER_DEF)
+        self.footer = PageTemplate(self.footer_file, self.FOOTER_DEF)
+
+    def check_updated(self, flist) :
+        self.header.check_updated(flist)
+        self.footer.check_updated(flist)
+
+    def set(self) :
+        self.header.set()
+        self.footer.set()
 
 class MDConv :
     md = None
@@ -244,13 +275,13 @@ class MDConv :
 
     def __init__(self) :
         if Markdown :
-            print("Use Markdown python module.")
+            print("Use Markdown python module to convert.")
             self.md = Markdown()
             self.conv = self.conv_py
         else :
             self.md_command = self.check_cmd("markdown.pl") or self.check_cmd("markdown")
             if self.md_command :
-                print("Use Markdown external command.")
+                print("Use command %s to onvert." % self.md_command)
                 self.conv = self.conv_pl
 
     def check_cmd(self, command) :
@@ -280,7 +311,6 @@ def help():
 
 def main(argv):
     mp = MDPage()
-    print("Current time is %s." % mp.cur_time)
     if len(argv) == 1 or argv[1] == "check" :
         mp.check()
         mp.print_updated()
