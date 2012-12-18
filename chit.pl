1caa72bc29e9d5562f43a1306930f7b888465866
10sr <8slashes+git@gmail.com>
2012-12-19 00:40:09 +0900

    add chit.pl

---
+++ chit.pl
@@ -0,0 +1,11 @@
+#!/usr/bin/env perl
+
+# very simple note taking app
+# basically no way to remove note
+# length of each note are limitted to **
+
+# usage :
+
+# chit -[a|c] [-d] <note>
+
+print "I am Chit!\n";
