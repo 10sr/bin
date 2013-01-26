429c9a592ea09e591cdd48fa742ed663d541e20e
10sr <8slashes+git@gmail.com>
2013-01-27 01:30:36 +0900

    dir should be created by rsync

--- backup.sh
+++ backup.sh
@@ -4,44 +4,55 @@ help(){
     cat <<'__EOC__' 2>&1
 backup: backup [file ...]
 
-Very simple backup tool by create directory named$dst/$timestr and copy files
+Very simple backup tool by create directory named $dst/$timestr and copy files
 into it.
 __EOC__
 }
 
-test -z "$dst" && dst="$HOME/.my/backup"
-timestr=`date +%Y%m%d-%H%M%S`
-if expr "$dst" : '.*:$' >/dev/null
+debug=
+
+defdst=".my/backup"
+if test -z "$dst"
 then
-    dstdir="$dst./$timestr"
+    dstdir="$HOME/$defdst"
+elif expr "$dst" : '.*:$' >/dev/null
+then
+    # only hostname is specified
+    dstdir="$dst./$defdst"
 else
-    dstdir="$dst/$timestr"
+    dstdir="$dst"
 fi
+timestr=`date +%Y%m%d-%H%M%S`
+dstdir="$dstdir/$timestr/"
 
 do_rsync(){
-    if expr "$dstdir" : '.*:' >/dev/null
-    then
-        host="$(expr "$dstdir" : '\(.*\):')"
-        dir="$(expr "$dstdir" : '.*:\(.*\)$')"
-        echo -n "Creating $dir in $host..."
-        ssh "$host" mkdir -p "$dir"
-        echo "done"
-    else
-        mkdir -p "$dstdir"
-    fi
+    # if expr "$dstdir" : '.*:' >/dev/null
+    # then
+    #     host="$(expr "$dstdir" : '\(.*\):')"
+    #     dir="$(expr "$dstdir" : '.*:\(.*\)$')"
+    #     printf "Creating $dir in $host..."
+    #     $debug ssh "$host" mkdir -p "$dir"
+    #     echo "done"
+    # else
+    #     $debug mkdir -p "$dstdir"
+    # fi
+
     # src=foo/ : copy the contents of this directory
     # src=foo : copy the directory by name
     # these two are same:
     #     rsync -av /src/foo  /dest
     #     rsync -av /src/foo/ /dest/foo
 
+    # if $dstdir ends with /
+    #     create dir $dstdir if not exists and copy files into $dstdir
     # if $dstdir already exists and it is a directory
     #     copy files into $dstdir
     # elif $@ is one file ($1)
     #     copy create file named $dstdir with contents of $1
     # else
     #     fail
-    rsync -a --stats --progress --human-readable "$@" "$dstdir"
+    echo "Start copying files into \"$dstdir\"."
+    $debug rsync -a --stats --progress --human-readable "$@" "$dstdir"
 }
 
 main(){
