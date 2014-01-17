b48882b80723eaf5bd5f8ff5966d9ab27c101745
10sr <8slashes+git@gmail.com>
2014-01-18 02:22:27 +0900

    fix dict_app.py

--- dict_app.py
+++ dict_app.py
@@ -3,11 +3,15 @@
 # http://sakito.jp/mac/dictionary.html
 
 import sys
-from DictionaryServices import *
+import DictionaryServices as ds
 
 def main():
     word = sys.argv[1].decode('utf-8')
-    result = DCSCopyTextDefinition(None, word, (0, len(word)))
+    # for e in dir(ds):
+    #     if e.startswith("DCS"):
+    #         print(e)
+    # return
+    result = ds.DCSCopyTextDefinition(None, word, (0, len(word)))
     print result.encode('utf-8')
 
 if __name__ == '__main__':
