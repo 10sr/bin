f7078778a5d3c8eceb1e667e5f28a375a733ee24
10sr <8slashes+git@gmail.com>
2014-01-19 00:32:47 +0900

    update googletranslate

--- junks/googletranslate.py
+++ junks/googletranslate.py
@@ -1,13 +1,23 @@
 #!/usr/bin/env python
 #coding:utf-8
 import urllib
-import simplejson
+import json
+
+URL = ("https://www.googleapis.com/language/translate/v2"
+       "?key={key}&target={target}&q={query}")
+KEY = "AIzaSyDrtCSkX5VZ3bxXz96Mur9P6olw96UYndw"
 
 def translate(query, frm="en", to="ja"):
     query = query.encode("utf-8")
-    data = {"q":query, "v":"1.0", "hl":"ja", "langpair":"%s|%s" % (frm, to),}
-    f = urllib.urlopen("http://ajax.googleapis.com/ajax/services/language/translate", urllib.urlencode(data))
-    ret = simplejson.loads(f.read())
-    return ret["responseData"]["translatedText"]
+    data = {
+        "query": query,
+        "key": KEY,
+        "target": "ja"
+    }
+    f = urllib.urlopen(URL.format(**data))
+    result = f.read()
+    print(result)
+    ret = json.loads(result)
+    return ret["data"] and ret["data"]["translations"][0]["translatedText"]
 
 print(translate("Dog"))
