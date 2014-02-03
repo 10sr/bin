8322b88bd49e50072c070885a21febe47cc9bf30
10sr <8slashes+git@gmail.com>
2014-02-04 02:40:33 +0900

    synk.sh: add some infomations

--- synk.sh
+++ synk.sh
@@ -22,6 +22,40 @@ set -e
 # and `synk push` do the otherwise.
 # Remaining options are directly passed to rsync.
 
+
+##############################################
+# synk --- Simple File Synchronization Utility
+
+# Auther: 10sr
+# URL: https://github.com/10sr/script/blob/master/synk.sh
+# LICENSE: Unlicense: http://unlicense.org
+
+# This is free and unencumbered software released into the public domain.
+
+# Anyone is free to copy, modify, publish, use, compile, sell, or
+# distribute this software, either in source code form or as a compiled
+# binary, for any purpose, commercial or non-commercial, and by any
+# means.
+
+# In jurisdictions that recognize copyright laws, the author or authors
+# of this software dedicate any and all copyright interest in the
+# software to the public domain. We make this dedication for the benefit
+# of the public at large and to the detriment of our heirs and
+# successors. We intend this dedication to be an overt act of
+# relinquishment in perpetuity of all present and future rights to this
+# software under copyright law.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+# IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+# OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+# ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+# OTHER DEALINGS IN THE SOFTWARE.
+
+# For more information, please refer to <http://unlicense.org/>
+
+
 conf=.synk.conf
 # rsync_default_opts="--archive --compress --stats --progress --human-readable"
 rsync_default_opts="--archive --progress --compress --human-readable"
