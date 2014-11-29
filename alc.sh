036ad21670d19a46e25329c2a3aa3522f2647ed0
10sr <8slashes+git@gmail.com>
2014-11-29 22:47:04 +0900

    alc.sh: Add some error handlings

--- alc.sh
+++ alc.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
-# src: http://taka.no32.tk/diary/20050826.html
+set -e
+# Original: http://taka.no32.tk/diary/20050826.html
+
+if ! which nkf >/dev/null
+then
+    echo nkf command not found
+    echo Abort.
+    exit 1
+fi
+
+if test -z "$1"
+then
+    cat <<__EOC__
+usage: alc.sh <word-or-phrases> ...
+__EOC__
+fi
+
 KEY=`echo $@ | nkf -w -w80`
 echo $@ >> ~/.eng-list # 履歴を保存（不要なら削除）．
 URI="http://eow.alc.co.jp/$KEY/UTF-8/"
